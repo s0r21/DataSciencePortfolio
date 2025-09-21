d4862263f800af7fbaa860c370a16cd56b9b18e0
Sorena Talebi <47501151+s0r21@users.noreply.github.com>
2025-09-20 19:16:07 -0600

    Update ReadMe.py

--- ReadMe.py
+++ ReadMe.py
@@ -4,3 +4,5 @@
     # Packages.py - Outlining all the packages that I'm using across all the py files.
     # DataToLoad.py - Built two Pandas dataframes that I'll be working with for the entire project.
     # NormalizationFunctions.py - Shows all the normalization functions that I've used in the past.
+    # PandasManipulation.py - Does some data wrangling using Pandas.
+    # Bootstrapping.py - Creates a random sample and using bootstrapping to get the averages to get a more central distribution.
